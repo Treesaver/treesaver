f5e9f5bf9040591fdf5d15ae4d6ec54982399ad8
Bram Stein <b.l.stein@gmail.com>
2010-12-05 01:09:28 +0100

    Replaced HTML5 shim with a slightly faster one.

--- src/lib/html5.js
+++ src/lib/html5.js
@@ -8,6 +8,6 @@ goog.provide('treesaver.html5');
 goog.require('treesaver.constants');
 
 if (SUPPORT_IE) {
-  // HTML5 shim, courtesy of: http://code.google.com/p/html5shim/
-  eval("/*@cc_on'abbr article aside audio canvas details figcaption figure footer header hgroup mark meter nav output progress section summary time video'.replace(/\w+/g,function(n){document.createElement(n)})@*/")
+  // HTML5 shim, courtesy of: https://github.com/lindsayevans/html5-shiv
+  eval("/*@cc_on (function(a,b,c){while(b--)a.createElement(c[b])})(document,21,['abbr','article','aside','audio','canvas','details','figcaption','figure','footer','header','hgroup','mark','menu','meter','nav','output','progress','section','summary','time','video'])@*/");
 }
