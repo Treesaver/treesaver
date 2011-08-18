2d0016a9c10009ee8331d9ae06f66cd1144d8ba5
Bram Stein <b.l.stein@gmail.com>
2011-08-18 19:07:12 +0200

    No sense in caching as it is only evaluated once as opposed to the Chrome.

--- src/layout/page.js
+++ src/layout/page.js
@@ -70,7 +70,6 @@ treesaver.layout.Page = function(content, grids, br) {
   treesaver.dimensions.setCssPx(this.node, 'height', this.size.h);
 
   treesaver.dom.getElementsByProperty('data-template', 'document', null, this.node).forEach(function (el) {
-    // FIXME: cache the compiled template
     el.innerHTML = Handlebars.compile(el.innerHTML)(content.doc.meta);
   });
 
