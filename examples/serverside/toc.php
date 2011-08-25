aae9fef637ef937d02ca8144ec99985f83173d86
Bram Stein <b.l.stein@gmail.com>
2011-08-25 20:31:15 +0200

    Fixed the examples.

--- examples/serverside/toc.php
+++ examples/serverside/toc.php
@@ -3,14 +3,10 @@
 header('Content-type: application/json');
 
 $toc = array(
-  array(
-    'url' => 'index.php'
-  ),
-  array(
-    'url' => '?article=1'
-  ),
-  array(
-    'url' => '?article=2'
+  'contents' => array(
+    'index.php',
+    '?article=1',
+    '?article=2'
   )
 );
 
