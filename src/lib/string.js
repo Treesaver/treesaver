4100be45565bc23c4e8eaf6b81cec2a0cfd880d3
Filipe Fortes <accounts@fortes.com>
2011-08-26 01:07:25 -0400

    Minor doc change in string module

--- src/lib/string.js
+++ src/lib/string.js
@@ -4,8 +4,9 @@
 
 goog.provide('treesaver.string');
 
-// Add string.trim() if it's not there
-if (!String.prototype.trim) {
+// Add string.trim() if it's not there, which happens in Safari pre-5 and
+// IE pre 9
+if (!'trim' in String.prototype) {
   String.prototype.trim = function() {
     return this.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
   };
