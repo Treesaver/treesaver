c130cf933155f31e68de8a82edc6d8158b41dc89
Filipe Fortes <accounts@fortes.com>
2011-08-26 01:34:50 -0400

    Update DOM tests

--- test/dom.js
+++ test/dom.js
@@ -35,14 +35,36 @@ $(function() {
     ok(treesaver.dom.hasAttr(div, 'width'), 'Property exists');
   });
 
-  test('getElementsByClassName', function () {
+  test('querySelectorAll', function () {
     var div = document.createElement('div');
     div.innerHTML = '<p class="one"><span class="one inner"></span></p>';
     div.innerHTML += '<p class="two"><span class="two inner"></span></p>';
 
-    equals(treesaver.dom.getElementsByClassName('one', div).length, 2);
-    equals(treesaver.dom.getElementsByClassName('inner', div).length, 2);
-    equals(treesaver.dom.getElementsByClassName('outer', div).length, 0);
+    // Converted from getElementsByClassName
+    equals(treesaver.dom.querySelectorAll('.one', div).length, 2);
+    equals(treesaver.dom.querySelectorAll('.inner', div).length, 2);
+    equals(treesaver.dom.querySelectorAll('.outer', div).length, 0);
+
+    // Converted from getElementsByProperty
+    div.innerHTML = '<p class="one" itemscope><span class="one inner"></span></p>';
+    div.innerHTML += '<p class="two"><span class="two inner"></span></p>';
+    div.innerHTML += '<p type="text/x-treesaver-template">hello world</p>';
+
+    equals(treesaver.dom.querySelectorAll('[class]', div).length, 4);
+    equals(treesaver.dom.querySelectorAll('[class~=one]', div).length, 2);
+    equals(treesaver.dom.querySelectorAll('p[class=one]', div).length, 1);
+    equals(treesaver.dom.querySelectorAll('[itemscope]', div).length, 1, 'Property without value');
+    equals(treesaver.dom.querySelectorAll('[type]', div).length, 1);
+
+    // Converted from getElementsByQuery
+    div.innerHTML = '<p>Hello <strong class="test">world</p>';
+
+    equals(treesaver.dom.querySelectorAll('p', div).length, 1);
+    equals(treesaver.dom.querySelectorAll('p, strong', div).length, 2);
+    equals(treesaver.dom.querySelectorAll('p,strong', div).length, 2);
+    equals(treesaver.dom.querySelectorAll('.test', div).length, 1);
+    equals(treesaver.dom.querySelectorAll('.test, p', div).length, 2);
+    equals(treesaver.dom.querySelectorAll('.test, p, strong, p', div).length, 2);
   });
 
   test('createDocumentFragmentFromHTML', function () {
@@ -52,38 +74,9 @@ $(function() {
 
   test('getElementsByProperty', function () {
     var div = document.createElement('div');
-    div.innerHTML = '<p class="one" itemscope><span class="one inner"></span></p>';
-    div.innerHTML += '<p class="two"><span class="two inner"></span></p>';
-    div.innerHTML += '<p type="text/x-treesaver-template">hello world</p>';
-
-    equals(treesaver.dom.getElementsByProperty('class', null, null, div).length, 4);
-    equals(treesaver.dom.getElementsByProperty('class', 'one', null, div).length, 2);
-    equals(treesaver.dom.getElementsByProperty('class', 'one', 'p', div).length, 1);
-    equals(treesaver.dom.getElementsByProperty('itemscope', null, null, div).length, 1, 'Property without value');
-    equals(treesaver.dom.getElementsByProperty('type', null, null, div).length, 1);
   });
 
   test('getElementsByQuery', function () {
     var div = document.createElement('div');
-    div.innerHTML = '<p>Hello <strong class="test">world</p>';
-
-    equals(treesaver.dom.getElementsByQuery('p', div).length, 1);
-    equals(treesaver.dom.getElementsByQuery('p, strong', div).length, 2);
-    equals(treesaver.dom.getElementsByQuery('p,strong', div).length, 2);
-    equals(treesaver.dom.getElementsByQuery('.test', div).length, 1);
-    equals(treesaver.dom.getElementsByQuery('.test, p', div).length, 2);
-    equals(treesaver.dom.getElementsByQuery('.test, p, strong, p', div).length, 2);
-  });
-
-  test('getAncestor', function () {
-    var div = document.createElement('div');
-    div.innerHTML = '<article id="top"><article id="article"><p id="text">Hello World</p></article></article>';
-
-    var p = treesaver.dom.getElementsByTagName('p', div)[0];
-
-    equals(treesaver.dom.getAncestor(p, 'ARTICLE').id, 'article', 'closest ancestor returned');
-    equals(treesaver.dom.getAncestor(p, 'article').id, 'article', 'closest ancestor returned (lowercase)');
-    equals(treesaver.dom.getAncestor(treesaver.dom.getAncestor(p, 'article'), 'article').id, 'top', 'retrieved two nearest ancestors');
-    equals(treesaver.dom.getAncestor(p, 'body'), null, 'correctly returned null on disconnected node');
   });
 });
