91edff02d0e6be79b8d04dfd7b25c3751e4df1ec
Filipe Fortes <accounts@fortes.com>
2011-08-26 01:06:56 -0400

    Lose IE support from styles and use goog.scope

--- src/lib/styles.js
+++ src/lib/styles.js
@@ -4,37 +4,36 @@
 
 goog.provide('treesaver.styles');
 
-goog.require('treesaver.capabilities');
-goog.require('treesaver.constants');
 goog.require('treesaver.debug');
 goog.require('treesaver.dom');
 
-/**
- * @param {!string} selector
- * @param {!string} text
- */
-treesaver.styles.insertRule = function(selector, text) {
-  if (!SUPPORT_IE || 'insertRule' in treesaver.styles.stylesheet_) {
-    treesaver.styles.stylesheet_.insertRule(selector + '{' + text + '}', 0);
+goog.scope(function() {
+  var styles = treesaver.styles,
+      debug = treesaver.debug,
+      dom = treesaver.dom;
+
+  /**
+   * @param {!string} selector
+   * @param {!string} text
+   */
+  styles.insertRule = function(selector, text) {
+    styles.stylesheet_.insertRule(selector + '{' + text + '}', 0);
+  }
+
+  styles.stylesheet_ = document.createElement('style');
+  styles.stylesheet_.setAttribute('type', 'text/css');
+
+  if (dom.querySelectorAll('head').length) {
+    dom.querySelectorAll('head')[0].appendChild(treesaver.styles.stylesheet_);
+    styles.stylesheet_ = document.styleSheets[document.styleSheets.length - 1];
+
+    // Offscreen
+    styles.insertRule('.offscreen',
+      'position:absolute;top:-200%;right:-200%;visibility:hidden;');
+    // Grids are centered in the viewer
+    styles.insertRule('.viewer .grid', 'top:50%;left:50%;margin:0');
   }
   else {
-    treesaver.styles.stylesheet_.addRule(selector, text);
+    debug.error("No head to put default stylesheet into");
   }
-}
-
-treesaver.styles.stylesheet_ = document.createElement('style');
-treesaver.styles.stylesheet_.setAttribute('type', 'text/css');
-
-if (treesaver.dom.querySelectorAll('head').length) {
-  treesaver.dom.querySelectorAll('head')[0].appendChild(treesaver.styles.stylesheet_);
-  treesaver.styles.stylesheet_ = document.styleSheets[document.styleSheets.length - 1];
-
-  // Offscreen
-  treesaver.styles.insertRule('.offscreen',
-    'position:absolute;top:-200%;right:-200%;visibility:hidden;');
-  // Grids are centered in the viewer
-  treesaver.styles.insertRule('.viewer .grid', 'top:50%;left:50%;margin:0');
-}
-else {
-  treesaver.debug.error("No head to put default stylesheet into");
-}
+});
