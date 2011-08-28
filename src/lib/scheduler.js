cbb0edd8a649e4839e471b203590c11a256b0430
Filipe Fortes <accounts@fortes.com>
2011-08-28 14:19:11 -0400

    Suspend task running if tickId is no longer present

--- src/lib/scheduler.js
+++ src/lib/scheduler.js
@@ -107,13 +107,11 @@ goog.scope(function() {
    */
   scheduler.tick_ = function() {
     var now = goog.now();
-    // Clear out previous id
-    scheduler.tickID_ = -1;
 
     scheduler.tasks_.forEach(function(task, i) {
       // If the tick function is no longer on interval, prevent all task
       // execution
-      if (!scheduler.tickID_) {
+      if (scheduler.tickID_ === -1) {
         return;
       }
 
@@ -156,6 +154,9 @@ goog.scope(function() {
 
       task.fun.apply(task.obj, task.args);
     });
+
+    // Clear out previous id
+    scheduler.tickID_ = -1;
 
     // Don't do anything if no tasks waiting
     if (scheduler.tasks_.length) {
