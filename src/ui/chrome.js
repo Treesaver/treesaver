58f11b0db4567c921b02ae8448f10911a1085c89
Filipe Fortes <accounts@fortes.com>
2011-08-28 21:24:39 -0400

    Make sure scheduled tasks resume when touch doesn't result in swipe

--- src/ui/chrome.js
+++ src/ui/chrome.js
@@ -824,7 +824,7 @@ treesaver.ui.Chrome.prototype.touchStart = function(e) {
   }
 
   // Pause other work for better swipe performance
-  treesaver.scheduler.pause([]);
+  treesaver.scheduler.pause(['resumeTasks']);
 };
 
 /**
