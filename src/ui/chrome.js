41ba65a34a87580a7fe7523540b69c99ab1a7c98
Andrea Campi <andrea.campi@zephirworks.com>
2012-02-09 16:12:11 +0100

    If the sidebar is a drawer, clicking elsewhere should not close it (#280).

--- src/ui/chrome.js
+++ src/ui/chrome.js
@@ -604,7 +604,9 @@ goog.scope(function() {
 
     if (!withinSidebar) {
       this.sidebars.forEach(function(sidebar) {
-        this.sidebarInactive(sidebar);
+        if (!dom.hasClass(sidebar, 'drawer')) {
+          this.sidebarInactive(sidebar);
+        }
       }, this);
     }
 
